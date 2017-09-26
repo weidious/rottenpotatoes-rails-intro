ea2c5bf678e92af4fc015a9c74cc40410c34af54
andrew <hunkywei@gmail.com>
2017-09-26 14:02:30 -0500

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,19 @@ class MoviesController < ApplicationController
   end
 
   def index
-  	@sort = params[:sort]
-	@movies = Movie.all.order(@sort)
+    @all_ratings = Movie.ratings
+    @sort = params[:sort] || session[:sort]
+    session[:ratings] = session[:ratings] || {'G'=>'','PG'=>'','PG-13'=>'','R'=>''}
+    #I consider they want to check out everything if user marks nothing
+    @t_param = params[:ratings] || session[:ratings] 
+    session[:sort] = @sort
+    session[:ratings] = @t_param 
+  #params[:ratings].nil? ? @t_param = @all_ratings : @t_param = params[:ratings].keys
+    @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
+    if(params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
+      flash.keep
+      redirect_to movies_path(sort: session[:sort],ratings: session[:ratings])
+    end
   end
 
   def new
